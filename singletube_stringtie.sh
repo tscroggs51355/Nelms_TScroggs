89fa34b90d2a783e10b69154602df88c4ab5a917
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-01-09 13:32:01 -0500

    dropped memory requirements, added in a cd at the beginning of the script and now retrying after I reran samtools sort in an independent script, still don't know why stringtie wasn't recgonizing that the files were sorted??

--- singletube_stringtie.sh
+++ singletube_stringtie.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=100gb
+#SBATCH --mem=70gb
 #SBATCH --time=72:00:00
 #SBATCH --output=NTS1_stringtie2.%j.out
 #SBATCH --error=NTS1_stringtie2.%j.err
@@ -11,12 +11,12 @@
 #SBATCH --mail-user=taylor.scroggs@uga.edu
 #SBATCH --export=NONE
 
-cd /scratch/tms51355/Taylor2024/NTS1
+cd /scratch/tms51355/Taylor2024/singletube_NTS1
 
 mkdir "Mapped_Data/stringtie_out"
 
 for file in "Mapped_Data/hisat2_out/"*.bam
 do
         module load StringTie/2.2.1-GCC-11.2.0
-        stringtie -p 4 -G /scratch/tms51355/Taylor2024/NTS1/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3 --rf -o "Mapped_Data/stringtie_out/""${file:22:-4}"".gtf" "$file"
+        stringtie -p 4 -G /scratch/tms51355/Taylor2024/singletube_NTS1/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3 --rf -o "Mapped_Data/stringtie_out/""${file:22:-4}"".gtf" "$file"
 done
