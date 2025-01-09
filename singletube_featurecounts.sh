48c010a5451a256d373cd197901482af412cd35e
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-01-09 17:15:54 -0500

    single tube feature counts

---
+++ singletube_featurecounts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=featurecounts_singletubeNTS1
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=70gb
+#SBATCH --time=72:00:00
+#SBATCH --output=FeatureCounts_singletubeNTS1_stringtie2.%j.out
+#SBATCH --error=FeatureCounts_singletubeNTS1_stringtie2.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=taylor.scroggs@uga.edu
+#SBATCH --export=NONE
+
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate subread-env
+
+# Loop through each BAM file in the directory
+for bam_file in /scratch/tms51355/Taylor2024/singletube_NTS1/Mapped_Data/hisat2_out/*.bam; do
+    # Get the filename without extension
+    filename=$(basename -- "$bam_file")
+    filename_no_ext="${filename%.*}"
+
+    # Run featureCounts for each BAM file
+    featureCounts -T 4 -s 1 -a "/scratch/tms51355/Taylor2024/singletube_NTS1/Mapped_Data/stringtie_out/stringtie_merged.gtf" \
+        -o "/scratch/tms51355/Taylor2024/singletube_NTS1/Mapped_Data/stringtie_out/${filename_no_ext}_read_counts.tab" \
+        --readExtension5 500 -R BAM "$bam_file"
+done
