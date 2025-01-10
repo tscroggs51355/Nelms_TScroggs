d79840175304d93dea7a9fdbb1cba148944f5bd9
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-01-10 16:38:09 -0500

    Now into R analysis on single tube samples, NTS1 samples are finishing up UMI counts in Sapelo2, need to be transferrred and normalized before sharing with Brad on Monday

--- NTS1_Analysis_2025.r
+++ NTS1_Analysis_2025.r
@@ -276,3 +276,66 @@ do
         # rm "$file"
     fi
 done
+
+###################
+C:\Users\taylo\Desktop\2025_Nelms\singletube_NTS1`
+
+setwd("C:/Users/taylo/Desktop/2025_Nelms/singletube_NTS1`/")
+annots = strsplit(read.table('Mapped_Data/stringtie_out/stringtie_merged.gtf', sep = '\t')[,9], '; ')
+names(annots) = unlist(lapply(annots, function(xx) { xx[1] }))
+names(annots) = sub('gene_id ', '', names(annots))
+annots = annots[!duplicated(names(annots))]
+annots = sub(';', '', sub(' ', '', unlist(lapply(annots, function(xx) { sub('.+ ', '', if (length(xx) == 3) { xx[3] } else { xx[1] }) }))))
+
+files = dir('Mapped_Data/UMIcounts')
+A0 = list()
+for (f in files) {
+  A0[[f]] = read.table(paste('Mapped_Data/UMIcounts/', f, sep = ''), sep = '\t', header=T, row.names=1)
+}
+
+gn = unique(unlist(lapply(A0, rownames)))
+A = matrix(NA, nrow = length(gn), ncol = length(files))
+rownames(A) = gn
+colnames(A) = files
+for (f in files) {
+  A[,f] = A0[[f]][match(gn,rownames(A0[[f]])),1]
+}
+
+colnames(A) <- sub("^(T\\d+)_.*\\.bam\\.tsv$", "\\1", colnames(A))
+colnames(A) # just check what everything is 
+A[is.na(A)] = 0
+A = A[rowSums(A) > 0,]
+rownames(A) = annots[rownames(A)]
+dim(A)
+[1] 27008     8
+
+A <- A[grepl("^Zm", rownames(A)), ]
+dim(A)
+[1] 24606     8
+
+## check for duplicated rownames with duplicated(rownames(A)) in base R studio 
+# can also use A[duplicated(rownames(A))] to see the exact positons of duplicates 
+
+for (g in unique(rownames(A)[duplicated(rownames(A))])) {
+    i = which(rownames(A) == g)
+    A[i[1],] = colSums(A[i,])
+    A = A[-i[-1],]
+}
+
+dim(A)
+[1] 24596     8
+
+pseudocount = 100
+B2 = A[,colSums(A) >= 5000]
+B3 = log(sweep(B2,2,colSums(B2),'/')*10^6 + pseudocount, 10)
+B4 = B3[rowSums(B2 >= 10) >= 2,]
+logTPM = t(scale(t(B4)))
+
+> summary(colSums(A))
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+ 171365  228084  290721  296514  341803  477746
+> summary(colSums(A > 0))
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+  14142   15873   16506   16458   17358   18271
+
+Heatmap(logTPM, show_row_names = F, row_km = 3)
