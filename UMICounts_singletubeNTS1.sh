2bea63e296f59002f042debb37c1ed80c314b4c9
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-01-10 13:48:57 -0500

    UMIcounts script for single tube NTS1 samples

---
+++ UMICounts_singletubeNTS1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=UMICounts_singletubeNTS1
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=70gb
+#SBATCH --time=72:00:00
+#SBATCH --output=UMICounts_singletubeNTS1_stringtie2.%j.out
+#SBATCH --error=UMICounts_singletubeNTS1_stringtie2.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=taylor.scroggs@uga.edu
+#SBATCH --export=NONE
+
+
+cd /scratch/tms51355/Taylor2024/singletube_NTS1
+mkdir "Mapped_Data/bams"
+mkdir "Mapped_Data/UMIcounts"
+
+for file in "Mapped_Data/stringtie_out/"*.bam
+do
+    file2="${file:26:-18}"
+    if [ ! -f "Mapped_Data/UMIcounts/${file2}.tsv" ]; then
+
+        module load SAMtools/1.16.1-GCC-11.3.0
+        samtools sort -@ 8 "$file" -o "Mapped_Data/bams/$file2"
+        samtools index "Mapped_Data/bams/$file2"
+
+        module load UMI-tools/1.1.2-foss-2022a-Python-3.10.4
+        umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS -I "Mapped_Data/bams/$file2" -S "Mapped_Data/UMIcounts/${file2}.tsv"
+        # rm "$file"
+    fi
+done
