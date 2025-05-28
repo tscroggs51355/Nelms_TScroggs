1ed11585799b0f6ba72a3593e212dd9be854b727
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-05-28 10:48:05 -0400

    StringtieReads

---
+++ Taylor2025/StringtieReads.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=stringtie2_out
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=75gb
+#SBATCH --time=72:00:00
+#SBATCH --output=stringtie2.%j.out
+#SBATCH --error=stringtie2.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=taylor.scroggs@uga.edu
+#SBATCH --export=NONE
+
+
+mkdir "Mapped_Data/stringtie_out"
+
+for file in "Mapped_Data/hisat2_out/"*s.bam
+do
+        module load StringTie/2.2.1-GCC-11.2.0
+        stringtie -p 6 -G /scratch/tms51355/Taylor2025/TS_March2025/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3 --rf -o "Mapped_Data/stringtie_out/""${file:22:-4}"".gtf" "$file"
+done
