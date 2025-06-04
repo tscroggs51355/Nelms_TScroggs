1b10a9de6cf5d0c1143dbf295f96004da607c214
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-06-04 12:57:18 -0400

    pUbUnspliced5 Check TS_march2025

---
+++ Taylor2025/pUbUnspliced5_PlasmidCheck_SH.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=pUbUnspliced5
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=70gb
+#SBATCH --time=72:00:00
+#SBATCH --output=pUbUnspliced5.%j.out
+#SBATCH --error=pUbUnspliced5.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=taylor.scroggs@uga.edu
+#SBATCH --export=NONE
+
+for fastq_file in filtered/*.fastq.gz; do
+
+    sample_name=$(basename "$fastq_file" .fastq.gz)
+
+    output_file="filtered/${sample_name}_pUbUnspliced5.txt"
+
+    zcat "$fastq_file" | sed -n '2~4p' | grep "TCCACCCGTCGGCACCTCCGCTTCAAGGTACGCCGCTCGTCCTCCCCCCC" | \
+        sed 's/TCCACCCGTCGGCACCTCCGCTTCAAGGTACGCCGCTCGTCCTCCCCCCC.*//' | \
+        grep -E '^.{30,}$' | head -n 1000 | awk '{print substr($0, length($0) - 30 + 1)}' | \
+        sort | uniq -c | sort -nr > "$output_file"
+done
+
