5aeebcf2671866b8df89002b2ab2334d75545af3
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-06-05 13:37:49 -0400

    pUbUnsplice3

--- Taylor2025/pUbUnspliced3_PlasmidCheck_SH.sh
+++ Taylor2025/pUbUnspliced3_PlasmidCheck_SH.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=pUbUnspliced5
+#SBATCH --job-name=pUbUnspliced3
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=70gb
 #SBATCH --time=72:00:00
-#SBATCH --output=pUbUnspliced5.%j.out
-#SBATCH --error=pUbUnspliced5.%j.err
+#SBATCH --output=pUbUnspliced3.%j.out
+#SBATCH --error=pUbUnspliced3.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=taylor.scroggs@uga.edu
 #SBATCH --export=NONE
@@ -15,11 +15,7 @@ for fastq_file in filtered/*.fastq.gz; do
 
     sample_name=$(basename "$fastq_file" .fastq.gz)
 
-    output_file="filtered/${sample_name}_pUbUnspliced5.txt"
+    output_file="filtered/${sample_name}_pUbUnspliced3.txt"
 
-    zcat "$fastq_file" | sed -n '2~4p' | grep "TCCACCCGTCGGCACCTCCGCTTCAAGGTACGCCGCTCGTCCTCCCCCCC" | \
-        sed 's/TCCACCCGTCGGCACCTCCGCTTCAAGGTACGCCGCTCGTCCTCCCCCCC.*//' | \
-        grep -E '^.{30,}$' | head -n 1000 | awk '{print substr($0, length($0) - 30 + 1)}' | \
-        sort | uniq -c | sort -nr > "$output_file"
+  zcat "$fastq_file" | sed -n '2~4p' | head -n 100000 | grep "CCCTGTTGTTTGGTGTTACTTCTGCAGGTCGACTCTAGAGGATCCCCTCG" | wc -l
 done
-
