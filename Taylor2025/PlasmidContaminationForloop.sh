5c17c4617459ee74ed48b56242e941f4fd669b19
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-06-06 13:38:48 -0400

    plasmid contamination script, updated with pUBsplice and unspliced

--- Taylor2025/PlasmidContaminationForloop.sh
+++ Taylor2025/PlasmidContaminationForloop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=GGGTGGGCGCG_NTS1_NTS1_1_PlasmidCheck
+#SBATCH --job-name=GGGTGGGCGC_PlasmidCheck_V2
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=70gb
 #SBATCH --time=72:00:00
-#SBATCH --output=PlasmidCheck_NTS1_NTS1_1.%j.out
-#SBATCH --error=PlasmidCheck_NTS1_NTS1_1.%j.err
+#SBATCH --output=GGGTGGGCGC_PlasmidCheck_V2.%j.out
+#SBATCH --error=GGGTGGGCGC_PlasmidCheck_V2.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=taylor.scroggs@uga.edu
 #SBATCH --export=NONE
@@ -16,8 +16,8 @@ for fastq_file in Mapped_Data/demultiplexed/*s.fastq.gz; do
     sample_name=$(basename "$fastq_file" .fastq.gz)
 
     zcat "$fastq_file" | sed -n '2~4p' | grep GGGTGGGCGCG | sed 's/GGGTGGGCGCG.*//' | \
-        grep -E '^.{30,}$' | head -n 1000 | awk '{print substr($0, length($0) - 30 + 1)}' | \
-        sort | uniq -c | sort -nr > "Mapped_Data/demultiplexed//${sample_name}.txt"
+        grep -E '^.{30,}$' | head -n 100000 | awk '{print substr($0, length($0) - 30 + 1)}' | \
+        sort | uniq -c | sort -nr > "Mapped_Data/demultiplexed//${sample_name}hundredthousand.txt"
 
 
 done
