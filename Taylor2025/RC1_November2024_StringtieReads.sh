50e052a201491b755b89f693a6a901cd42565724
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-06-23 16:16:15 -0400

    type with extra slash in genome

--- Taylor2025/RC1_November2024_StringtieReads.sh
+++ Taylor2025/RC1_November2024_StringtieReads.sh
@@ -18,5 +18,5 @@ mkdir "Mapped_Data/stringtie_out"
 for file in "Mapped_Data/hisat2_out/"*s.bam
 do
         module load StringTie/2.2.1-GCC-11.2.0
-        stringtie -p 6 -G //scratch/tms51355/Taylor2024/RC1_Nov2024/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3 --rf -o "Mapped_Data/stringtie_out/""${file:22:-4}"".gtf" "$file"
+        stringtie -p 6 -G /scratch/tms51355/Taylor2024/RC1_Nov2024/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3 --rf -o "Mapped_Data/stringtie_out/""${file:22:-4}"".gtf" "$file"
 done
