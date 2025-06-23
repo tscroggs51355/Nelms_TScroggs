ccf7e1a760fbf5119f33ffddd41e24cf0da2210c
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-06-23 15:44:48 -0400

    RC1 November 2024 FeatureCounts

---
+++ Taylor2025/RC1_November2024_FeatureCounts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=featurecounts_RC1_November2024
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=70gb
+#SBATCH --time=72:00:00
+#SBATCH --output=FeatureCounts_RC1_November2024_stringtie2.%j.out
+#SBATCH --error=FeatureCounts_RC1_November2024_stringtie2.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=taylor.scroggs@uga.edu
+#SBATCH --export=NONE
+
+cd /scratch/tms51355/Taylor2024/RC1_Nov2024
+
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate subread-env
+
+# Loop through each BAM file in the directory
+for gtf_file in /scratch/tms51355/Taylor2024/RC1_Nov2024/Mapped_Data/stringtie_out/*.gtf; do
+    # Get the filename without extension
+    filename=$(basename -- "$gtf_file")
+    filename_no_ext="${filename%.*}"
+
+# Run featureCounts on all BAM files together
+featureCounts -T 6 -s 1 -a "Mapped_Data/stringtie_out/stringtie_merged.gtf" -o "Mapped_Data/stringtie_out/read_counts.tab" --readExtension5 500 -R BAM "Mapped_Data/hisat2_out/"*.bam
