3eddbfece0519287ab7dc5fc9ccd28f3dae7ebbf
Taylor Scroggs <taylor.scroggs@uga.edu>
2025-08-18 16:53:52 -0400

    demultiplexing and mapping added to master script

--- Taylor2025/July2025_NTS2_NTS2_MasterScript.sh
+++ Taylor2025/July2025_NTS2_NTS2_MasterScript.sh
@@ -1,3 +1,54 @@
+## Demultiplexing 
+# Activate your Conda environment here if needed
+source activate /home/tms51355/Fastq-Multx/
+
+cd $SLURM_SUBMIT_DIR
+
+
+for file in Raw_Data/*_R1_*.gz; do
+    filename=$(basename "$file")
+    file2=$(echo "$filename" | sed 's/_R1.*//' | sed 's/_R2_001.fastq.gz//')
+
+if [ ! -f "Mapped_Data/demultiplexed/""$file2""_dT-1s.fastq.gz" ]; then
+module load fastp/0.23.2
+	fastp -w 4 -i "$file" -I "Raw_Data/""$file2""_R2_001.fastq.gz" -o "Mapped_Data/demultiplexed/umi_""$file2""_R1.fastq.gz" -O "Mapped_Data/demultiplexed/umi_""$file2""_R2.fastq.gz" -A -Q -L -G --umi --umi_loc read2 --umi_len 10 --umi_prefix UMI
+
+	fastq-multx -b -B "/scratch/tms51355/Taylor2025/TS_March2025/CELSeq_barcodes" -m 0 "Mapped_Data/demultiplexed/umi_""$file2""_R2.fastq.gz" "Mapped_Data/demultiplexed/umi_""$file2""_R1.fastq.gz" -o "Mapped_Data/demultiplexed/""$file2""_%_R2.fastq.gz" "Mapped_Data/demultiplexed/""$file2""_%.fastq.gz"  
+
+fi
+done
+conda deactivate
+
+#############################################################################333
+### Mapping 
+cd /scratch/tms51355/Taylor2025/July2025Sequencing_NTS2
+
+mkdir "Mapped_Data/hisat2_out"
+
+
+for file in "Mapped_Data/demultiplexed/"*s.fastq*
+do
+        file2="${file:26:-9}"
+
+if [ ! -f "Mapped_Data/hisat2_out/""$file2"".bam" ]; then
+
+        module load fastp/0.23.2-GCC-11.2.0
+        fastp -w 8 -i "$file" -o "Mapped_Data/hisat2_out/""$file2"".fastq.gz" -y -x -3 -a AAAAAAAAAAAA
+
+        module load HISAT2/2.2.1-gompi-2022a
+        module load SAMtools/1.16.1-GCC-11.3.0
+        hisat2 -p 8  -x /scratch/tms51355/Taylor2025/July2025Sequencing_NTS2/maize_tran -U "Mapped_Data/hisat2_out/""$file2"".fastq.gz" | samtools view -bS -> "Mapped_Data/hisat2_out/""$file2""_unsorted.bam"
+
+        module load SAMtools/1.16.1-GCC-11.3.0
+        samtools sort -@ 8 "Mapped_Data/hisat2_out/""$file2""_unsorted.bam" -o "Mapped_Data/hisat2_out/""$file2"".bam"
+fi
+done
+
+
+
+
+
+
 ###################################################################################
 ###################### FeautureCounts 
 #!/bin/bash
